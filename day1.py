a6973c0ed6f9e218421ef733654ccb460c7a355d
AmbientNomad <73661131+AmbientNomad@users.noreply.github.com>
2021-01-27 14:38:27 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -4,7 +4,7 @@ with open("./day1input.txt") as day1_input:
     modules = [int(EachLine.rstrip()) for EachLine in day1_input]
 
 
-def additional_fuel(fuel, required_fuel): # This function only for Part 2, Part 1 is solved in the sum function below
+def additional_fuel(fuel, required_fuel): # This function only for Part 2, Part 1 is solved in the sum() function below
     module_fuel = floor(fuel / 3) - 2
 
     if module_fuel > 0:
